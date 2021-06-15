e364b3d3cd2f602aa6b0802604c116da1a75845c
Archana550 <51438542+Archana550@users.noreply.github.com>
2021-06-15 21:25:43 +0530

    second commit

---
+++ src/core/Base.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Menu from "./Menu";
+
+
+
+const Base = ({
+  title = "My Title",
+  description = "My desription",
+  className = "bg-dark text-white p-4 padding-foo",
+  children
+}) => (
+  <div>
+  <Menu />
+    <div className="container-fluid">
+      <div className="jumbotron bg-dark text-white text-center">
+        <h2 className="display-4">{title}</h2>
+        <p className="lead">{description}</p>
+      </div>
+      <div className={className}>{children}</div>
+    </div>
+    <footer className="footer bg-dark mt-auto py-3">
+    <span className="text-muted">
+          An Amazing <span className="text-white">BLOGGING</span> application
+        </span>
+      <div className="container-fluid bg-success text-white text-center py-5 padding">
+        <h4>Made with ❤ by Archana & team. We would be thankful if you can share your valuable reviews here.</h4>
+        <Link to="/contact"><button className="btn btn-info">Contact Us 😊</button></Link>
+      </div>
+      
+        
+      
+    </footer>
+  </div>
+);
+
+export default Base;
---
+++ src/core/Home.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import "../styles.css";
+import Base from "./Base";
+import Card from "./Card";
+import { getPosts } from "./helper/coreapicalls";
+
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadAllPosts = () => {
+    getPosts().then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setPosts(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadAllPosts();
+  }, []);
+
+  return (
+    <Base title="Welcome To My Blog🎉" description="Let's get started">
+      <div className="row text-center">
+        <div className="row">
+          {posts.map((posts, index) => {
+            return (
+              <div key={index} className="col-4 mb-4">
+              <p>{index +1}.</p>
+                <Card post={posts} />
+                <hr />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </Base>
+  );
+};
+
+export default Home;
